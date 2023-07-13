eae29844dae285bee69486360535d2aa09d80e4e
Maxime Huguet~ <huguet.maxime@gmail.com>
2023-07-13 12:58:09 +0200

    connection string

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
-const connectionString = process.env.DB_CONNECTION_STRING
+const connectionString = process.env.CONNECTION_STRING
 mongoose.set("strictQuery", true)
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log("Successfully connected to the Tablée Database 🥳 !"))
+  .then(() => console.log("Successfully connected to the Database"))
   .catch((errorMessage) => console.error(errorMessage))
