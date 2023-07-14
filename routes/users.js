d07c15852a29c5ce8685c7ac5c534024fca79dc4
Maxime Huguet~ <huguet.maxime@gmail.com>
2023-07-14 15:22:16 +0200

    modif route user

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.post('/signup', (req, res) => {
       });
 
       newUser.save().then(newDoc => {
-        res.json({ result: true, token: newDoc.token, username:newDoc.username, firstname: newDoc.firstname });
+        res.json({ result: true, token: newDoc.token, username:newDoc.username, firstname: newDoc.firstname, userId : newDoc._id});
       });
     } else {
       // User already exists in database
@@ -48,12 +48,19 @@ router.post('/signin', (req, res) => {
 
   User.findOne({ username: req.body.username }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token, firstname: data.firstname, username: data.username });
+      res.json({ result: true, token: data.token, firstname: data.firstname, username: data.username, userId : data._id});
     } else {
       res.json({ result: false, error: 'User not found' });
     }
   });
 });
+
+router.get('/', (req, res) => {
+  User.findOne({token: req.body.token})
+  .then(user => {
+    res.json({result: true, user : user})
+  })
+})
 
 
 
